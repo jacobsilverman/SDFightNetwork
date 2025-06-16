04e94c0f5b6d91b34b4cc6b587fc2b6b98742c57
Jacob Silverman <jacob.h.silverman@gmail.com>
2025-06-16 16:48:45 -0700

    fixing the fighters data and kick image on about us page

---
+++ src/data/Fighters.jsx
@@ -0,0 +1,50 @@
+import fighter1 from "../assets/fighter1.png";
+import fighter1Animation from "../assets/fighter1-animation.gif";
+import fighter2 from "../assets/fighter2.png";
+import fighter2Animation from "../assets/fighter2-animation.gif";
+import fighter3 from "../assets/fighter3.png";
+import fighter3Animation from "../assets/fighter3-animation.gif";
+
+export const fightersData = {
+  "fighters": [
+    {
+      "id": 1,
+      "name": "Alex Johnson",
+      "height": "5'10\"",
+      "weight": "170 lbs",
+      "fightingStyles": ["Boxing", "BJJ"],
+      "age": 28,
+      "yearsTraining": 5,
+      "location": "San Diego, CA",
+      "gym": "North Park MMA",
+      "profileImage": fighter1,
+      "profileGif": fighter1Animation
+    },
+    {
+      "id": 2,
+      "name": "Steve Hoffman",
+      "height": "5'7\"",
+      "weight": "135 lbs",
+      "fightingStyles": ["Muay Thai", "MMA"],
+      "age": 44,
+      "yearsTraining": 3,
+      "location": "La Jolla, CA",
+      "gym": "Shogun Combat Club",
+      "profileImage": fighter2,
+      "profileGif": fighter2Animation
+    },
+    {
+      "id": 3,
+      "name": "Cambell Ownens",
+      "height": "6'1\"",
+      "weight": "200 lbs",
+      "fightingStyles": ["Muay Thai", "MMA"],
+      "age": 32,
+      "yearsTraining": 5,
+      "location": "La Jolla, CA",
+      "gym": "Shogun Combat Club",
+      "profileImage": fighter3,
+      "profileGif": fighter3Animation
+    }
+  ]
+}
--- src/pages/Fighters/Fighters.jsx
+++ src/pages/Fighters/Fighters.jsx
@@ -1,57 +1,9 @@
 import React, { useState } from 'react';
 import './Fighters.scss';
-import fighter1 from "../../assets/fighter1.png";
-import fighter2 from "../../assets/fighter2.png";
-import fighter3 from "../../assets/fighter3.png";
-import fighter1Animation from "../../assets/fighter1-animation.gif";
-import fighter2Animation from "../../assets/fighter2-animation.gif";
-import fighter3Animation from "../../assets/fighter3-animation.gif";
-
-const dummyFighters = [
-  {
-    id: 1,
-    name: 'Alex Johnson',
-    height: "5'10\"",
-    weight: '170 lbs',
-    fightingStyles: ['Boxing', 'BJJ'],
-    age: 28,
-    yearsTraining: 5,
-    location: 'San Diego, CA',
-    gym: 'North Park MMA',
-    profileImage: fighter1,
-    profileGif: fighter1Animation
-  },
-  {
-    id: 2,
-    name: 'Steve Hoffman',
-    height: "5'7\"",
-    weight: '135 lbs',
-    fightingStyles: ['Muay Thai', 'MMA'],
-    age: 44,
-    yearsTraining: 3,
-    location: 'La Jolla, CA',
-    gym: 'Shogun Combat Club',
-    profileImage: fighter2,
-    profileGif: fighter2Animation
-  },
-  {
-    id: 3,
-    name: 'Cambell Ownens',
-    height: "6'1\"",
-    weight: '200 lbs',
-    fightingStyles: ['Muay Thai', 'MMA'],
-    age: 32,
-    yearsTraining: 5,
-    location: 'La Jolla, CA',
-    gym: 'Shogun Combat Club',
-    profileImage: fighter3,
-    profileGif: fighter3Animation
-  },
-  // Add more fighters as needed
-];
+import {fightersData} from "../../data/Fighters.jsx";
 
 export default function Fighters() {
-  const [fighters, setFighters] = useState(dummyFighters);
+  const [fighters, setFighters] = useState(fightersData.fighters);
   const [filters, setFilters] = useState({
     fightingStyle: '',
     location: '',
